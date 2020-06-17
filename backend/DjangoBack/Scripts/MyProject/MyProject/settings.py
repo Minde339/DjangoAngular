3c45b1107ea6edc969b21cc57828879176d0d858
Minde339 <58472290+Minde339@users.noreply.github.com>
2020-06-17 15:49:28 +0300

    Update settings.py

--- backend/DjangoBack/Scripts/MyProject/MyProject/settings.py
+++ backend/DjangoBack/Scripts/MyProject/MyProject/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3zh^0xp(fpqu-(!^aibxptamslnw8f+wj4=3@0a2f-)-x8l%=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
